14e3dcdcd1cb88fb6cba66911bfb415cdce530c2
Zack <zphixon@gmail.com>
2022-02-23 19:56:24 -0500

    do websockets instead

--- app/src/main/java/com/example/testapp/SecurityApplication.java
+++ app/src/main/java/com/example/testapp/SecurityApplication.java
@@ -29,7 +29,13 @@ import com.amazonaws.regions.Regions;
 import com.amazonaws.services.cognitoidentity.model.CognitoIdentityProvider;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
+import com.neovisionaries.ws.client.WebSocket;
+import com.neovisionaries.ws.client.WebSocketAdapter;
+import com.neovisionaries.ws.client.WebSocketException;
+import com.neovisionaries.ws.client.WebSocketFactory;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 
 public class SecurityApplication extends Application {
@@ -45,7 +51,6 @@ public class SecurityApplication extends Application {
     public static final String CUSTOMER_SPECIFIC_ENDPOINT = "aw62mbu5dp5po-ats.iot.us-east-1.amazonaws.com";
     public static final String COGNITO_USER_POOL_ID = "us-east-1_KgdGVvm5L";
     public static final String COGNITO_IDENTITY_POOL_ID = "us-east-1:11c5508c-25db-4d40-baf1-6e15da71b02a";
-
 
     private static final Regions MY_REGION = Regions.US_EAST_1;
     public static RequestQueue requestQueue;
@@ -63,6 +68,22 @@ public class SecurityApplication extends Application {
         StrictMode.ThreadPolicy bruh = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(bruh);
 
+        WebSocketFactory factory = new WebSocketFactory();
+        try {
+            //WebSocket ws = factory.createSocket("ws://xn--yh8hfqgj.ws:8765/");
+            WebSocket ws = factory.createSocket("ws://10.0.2.2:8765/");
+            ws.addListener(new WebSocketAdapter() {
+                @Override
+                public void onTextMessage(WebSocket socket, String message) throws Exception {
+                    Log.e(TAG, "yoooo " + message);
+                }
+            });
+            ws.connect();
+            ws.sendText("waddup");
+            Log.e(TAG, "nice?");
+        } catch (IOException | WebSocketException e) {
+            Log.e(TAG, "" + e);
+        }
 
         // Initialize the AWS Cognito credentials provider
         credentialsProvider = new CognitoCachingCredentialsProvider(
